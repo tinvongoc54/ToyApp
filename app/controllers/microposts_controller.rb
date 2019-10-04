67dda47414c12d686f0546f797f968d9b1134434
tinvongoc54 <tin.vongoc.54@gmail.com>
2019-10-04 20:15:39 +0700

    create db

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,23 @@
+class MicropostsController < ApplicationController
+    def index
+        @microposts = Micropost.all
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def delete
+    end
+end
